965a21d3a48c4ae329d488dec61af44451a1e0ea
Paul Gillard <paulmgillard@gmail.com>
2010-09-19 16:13:10 +0100

    Add: HSL colour tests
    Test for varying saturation and lightness

--- test/unit/HSL.js
+++ test/unit/HSL.js
@@ -14,7 +14,7 @@ test("black", function() {
 
 // Red
 
-test("red", function() {
+test("pure red", function() {
   colour = new HSL(hue = 0, saturation = 1, lightness = 0.5);
   equals(colour.hue, hue, "Hue value");
   equals(colour.saturation, saturation, "Saturation value");
@@ -24,9 +24,64 @@ test("red", function() {
   same(colour.toRGB(opacity = 0.6), new RGB(255, 0, 0, opacity), "Explicitly translucent RGB value");
 });
 
+test("high saturation red", function() {
+  colour = new HSL(hue = 0, saturation = 0.75, lightness = 0.5);
+  same(colour.toRGB(), new RGB(223, 32, 32), "RGB value");
+});
+
+test("low saturation red", function() {
+  colour = new HSL(hue = 0, saturation = 0.25, lightness = 0.5);
+  same(colour.toRGB(), new RGB(159, 96, 96), "RGB value");
+});
+
+test("zero saturation red", function() {
+  colour = new HSL(hue = 0, saturation = 0, lightness = 0.5);
+  same(colour.toRGB(), new RGB(128, 128, 128), "RGB value");
+});
+
+test("full lightness red", function() {
+  colour = new HSL(hue = 0, saturation = 1, lightness = 1);
+  same(colour.toRGB(), new RGB(255, 255, 255), "RGB value");
+});
+
+test("high lightness red", function() {
+  colour = new HSL(hue = 0, saturation = 1, lightness = 0.75);
+  same(colour.toRGB(), new RGB(255, 128, 128), "RGB value");
+});
+
+test("low lightness red", function() {
+  colour = new HSL(hue = 0, saturation = 1, lightness = 0.25);
+  same(colour.toRGB(), new RGB(128, 0, 0), "RGB value");
+});
+
+test("zero lightness red", function() {
+  colour = new HSL(hue = 0, saturation = 1, lightness = 0);
+  same(colour.toRGB(), new RGB(0, 0, 0), "RGB value");
+});
+
+test("light strong tonal red", function() {
+  colour = new HSL(hue = 0, saturation = 0.75, lightness = 0.75);
+  same(colour.toRGB(), new RGB(239, 143, 143), "RGB value");
+});
+
+test("light weak tonal red", function() {
+  colour = new HSL(hue = 0, saturation = 0.25, lightness = 0.875);
+  same(colour.toRGB(), new RGB(231, 215, 215), "RGB value");
+});
+
+test("dark strong tonal red", function() {
+  colour = new HSL(hue = 0, saturation = 0.75, lightness = 0.125);
+  same(colour.toRGB(), new RGB(56, 8, 8), "RGB value");
+});
+
+test("dark weak tonal red", function() {
+  colour = new HSL(hue = 0, saturation = 0.25, lightness = 0.25);
+  same(colour.toRGB(), new RGB(80, 48, 48), "RGB value");
+});
+
 // Yellow
 
-test("yellow", function() {
+test("pure yellow", function() {
   colour = new HSL(hue = 60, saturation = 1, lightness = 0.5);
   equals(colour.hue, hue, "Hue value");
   equals(colour.saturation, saturation, "Saturation value");
@@ -36,9 +91,64 @@ test("yellow", function() {
   same(colour.toRGB(opacity = 0.9), new RGB(255, 255, 0, opacity), "Explicitly translucent RGB value");
 });
 
+test("high saturation yellow", function() {
+  colour = new HSL(hue = 60, saturation = 0.75, lightness = 0.5);
+  same(colour.toRGB(), new RGB(223, 223, 32), "RGB value");
+});
+
+test("low saturation yellow", function() {
+  colour = new HSL(hue = 60, saturation = 0.25, lightness = 0.5);
+  same(colour.toRGB(), new RGB(159, 159, 96), "RGB value");
+});
+
+test("zero saturation yellow", function() {
+  colour = new HSL(hue = 60, saturation = 0, lightness = 0.5);
+  same(colour.toRGB(), new RGB(128, 128, 128), "RGB value");
+});
+
+test("full lightness yellow", function() {
+  colour = new HSL(hue = 60, saturation = 1, lightness = 1);
+  same(colour.toRGB(), new RGB(255, 255, 255), "RGB value");
+});
+
+test("high lightness yellow", function() {
+  colour = new HSL(hue = 60, saturation = 1, lightness = 0.875);
+  same(colour.toRGB(), new RGB(255, 255, 191), "RGB value");
+});
+
+test("low lightness yellow", function() {
+  colour = new HSL(hue = 60, saturation = 1, lightness = 0.125);
+  same(colour.toRGB(), new RGB(64, 64, 0), "RGB value");
+});
+
+test("zero lightness yellow", function() {
+  colour = new HSL(hue = 60, saturation = 1, lightness = 0);
+  same(colour.toRGB(), new RGB(0, 0, 0), "RGB value");
+});
+
+test("light strong tonal yellow", function() {
+  colour = new HSL(hue = 60, saturation = 0.75, lightness = 0.875);
+  same(colour.toRGB(), new RGB(247, 247, 199), "RGB value");
+});
+
+test("light weak tonal yellow", function() {
+  colour = new HSL(hue = 60, saturation = 0.25, lightness = 0.75);
+  same(colour.toRGB(), new RGB(207, 207, 175), "RGB value");
+});
+
+test("dark strong tonal yellow", function() {
+  colour = new HSL(hue = 60, saturation = 0.75, lightness = 0.25);
+  same(colour.toRGB(), new RGB(112, 112, 16), "RGB value");
+});
+
+test("dark weak tonal yellow", function() {
+  colour = new HSL(hue = 60, saturation = 0.25, lightness = 0.25);
+  same(colour.toRGB(), new RGB(80, 80, 48), "RGB value");
+});
+
 // Green
 
-test("green", function() {
+test("pure green", function() {
   colour = new HSL(hue = 120, saturation = 1, lightness = 0.5);
   equals(colour.hue, hue, "Hue value");
   equals(colour.saturation, saturation, "Saturation value");
@@ -48,9 +158,64 @@ test("green", function() {
   same(colour.toRGB(opacity = 0.9), new RGB(0, 255, 0, opacity), "Explicitly translucent RGB value");
 });
 
+test("high saturation green", function() {
+  colour = new HSL(hue = 120, saturation = 0.75, lightness = 0.5);
+  same(colour.toRGB(), new RGB(32, 223, 32), "RGB value");
+});
+
+test("low saturation green", function() {
+  colour = new HSL(hue = 120, saturation = 0.25, lightness = 0.5);
+  same(colour.toRGB(), new RGB(96, 159, 96), "RGB value");
+});
+
+test("zero saturation green", function() {
+  colour = new HSL(hue = 120, saturation = 0, lightness = 0.5);
+  same(colour.toRGB(), new RGB(128, 128, 128), "RGB value");
+});
+
+test("full lightness green", function() {
+  colour = new HSL(hue = 120, saturation = 1, lightness = 1);
+  same(colour.toRGB(), new RGB(255, 255, 255), "RGB value");
+});
+
+test("high lightness green", function() {
+  colour = new HSL(hue = 120, saturation = 1, lightness = 0.75);
+  same(colour.toRGB(), new RGB(128, 255, 128), "RGB value");
+});
+
+test("low lightness green", function() {
+  colour = new HSL(hue = 120, saturation = 1, lightness = 0.125);
+  same(colour.toRGB(), new RGB(0, 64, 0), "RGB value");
+});
+
+test("zero lightness green", function() {
+  colour = new HSL(hue = 120, saturation = 1, lightness = 0);
+  same(colour.toRGB(), new RGB(0, 0, 0), "RGB value");
+});
+
+test("light strong tonal green", function() {
+  colour = new HSL(hue = 120, saturation = 0.75, lightness = 0.75);
+  same(colour.toRGB(), new RGB(143, 239, 143), "RGB value");
+});
+
+test("light weak tonal green", function() {
+  colour = new HSL(hue = 120, saturation = 0.25, lightness = 0.875);
+  same(colour.toRGB(), new RGB(215, 231, 215), "RGB value");
+});
+
+test("dark strong tonal green", function() {
+  colour = new HSL(hue = 120, saturation = 0.75, lightness = 0.125);
+  same(colour.toRGB(), new RGB(8, 56, 8), "RGB value");
+});
+
+test("dark weak tonal green", function() {
+  colour = new HSL(hue = 120, saturation = 0.25, lightness = 0.25);
+  same(colour.toRGB(), new RGB(48, 80, 48), "RGB value");
+});
+
 // Cyan
 
-test("cyan", function() {
+test("pure cyan", function() {
   colour = new HSL(hue = 180, saturation = 1, lightness = 0.5);
   equals(colour.hue, hue, "Hue value");
   equals(colour.saturation, saturation, "Saturation value");
@@ -60,9 +225,64 @@ test("cyan", function() {
   same(colour.toRGB(opacity = 0.9), new RGB(0, 255, 255, opacity), "Explicitly translucent RGB value");
 });
 
+test("high saturation cyan", function() {
+  colour = new HSL(hue = 180, saturation = 0.75, lightness = 0.5);
+  same(colour.toRGB(), new RGB(32, 223, 223), "RGB value");
+});
+
+test("low saturation cyan", function() {
+  colour = new HSL(hue = 180, saturation = 0.25, lightness = 0.5);
+  same(colour.toRGB(), new RGB(96, 159, 159), "RGB value");
+});
+
+test("zero saturation cyan", function() {
+  colour = new HSL(hue = 180, saturation = 0, lightness = 0.5);
+  same(colour.toRGB(), new RGB(128, 128, 128), "RGB value");
+});
+
+test("full lightness cyan", function() {
+  colour = new HSL(hue = 180, saturation = 1, lightness = 1);
+  same(colour.toRGB(), new RGB(255, 255, 255), "RGB value");
+});
+
+test("high lightness cyan", function() {
+  colour = new HSL(hue = 180, saturation = 1, lightness = 0.625);
+  same(colour.toRGB(), new RGB(64, 255, 255), "RGB value");
+});
+
+test("low lightness cyan", function() {
+  colour = new HSL(hue = 180, saturation = 1, lightness = 0.375);
+  same(colour.toRGB(), new RGB(0, 191, 191), "RGB value");
+});
+
+test("zero lightness cyan", function() {
+  colour = new HSL(hue = 180, saturation = 1, lightness = 0);
+  same(colour.toRGB(), new RGB(0, 0, 0), "RGB value");
+});
+
+test("light strong tonal cyan", function() {
+  colour = new HSL(hue = 180, saturation = 0.75, lightness = 0.625);
+  same(colour.toRGB(), new RGB(88, 231, 231), "RGB value");
+});
+
+test("light weak tonal cyan", function() {
+  colour = new HSL(hue = 180, saturation = 0.25, lightness = 0.75);
+  same(colour.toRGB(), new RGB(175, 207, 207), "RGB value");
+});
+
+test("dark strong tonal cyan", function() {
+  colour = new HSL(hue = 180, saturation = 0.75, lightness = 0.125);
+  same(colour.toRGB(), new RGB(8, 56, 56), "RGB value");
+});
+
+test("dark weak tonal cyan", function() {
+  colour = new HSL(hue = 180, saturation = 0.25, lightness = 0.375);
+  same(colour.toRGB(), new RGB(72, 120, 120), "RGB value");
+});
+
 // Blue
 
-test("blue", function() {
+test("pure blue", function() {
   colour = new HSL(hue = 240, saturation = 1, lightness = 0.5);
   equals(colour.hue, hue, "Hue value");
   equals(colour.saturation, saturation, "Saturation value");
@@ -72,9 +292,64 @@ test("blue", function() {
   same(colour.toRGB(opacity = 0.9), new RGB(0, 0, 255, opacity), "Explicitly translucent RGB value");
 });
 
+test("high saturation blue", function() {
+  colour = new HSL(hue = 240, saturation = 0.75, lightness = 0.5);
+  same(colour.toRGB(), new RGB(32, 32, 223), "RGB value");
+});
+
+test("low saturation blue", function() {
+  colour = new HSL(hue = 240, saturation = 0.25, lightness = 0.5);
+  same(colour.toRGB(), new RGB(96, 96, 159), "RGB value");
+});
+
+test("zero saturation blue", function() {
+  colour = new HSL(hue = 240, saturation = 0, lightness = 0.5);
+  same(colour.toRGB(), new RGB(128, 128, 128), "RGB value");
+});
+
+test("full lightness blue", function() {
+  colour = new HSL(hue = 240, saturation = 1, lightness = 1);
+  same(colour.toRGB(), new RGB(255, 255, 255), "RGB value");
+});
+
+test("high lightness blue", function() {
+  colour = new HSL(hue = 240, saturation = 1, lightness = 0.625);
+  same(colour.toRGB(), new RGB(64, 64, 255), "RGB value");
+});
+
+test("low lightness blue", function() {
+  colour = new HSL(hue = 240, saturation = 1, lightness = 0.375);
+  same(colour.toRGB(), new RGB(0, 0, 191), "RGB value");
+});
+
+test("zero lightness blue", function() {
+  colour = new HSL(hue = 240, saturation = 1, lightness = 0);
+  same(colour.toRGB(), new RGB(0, 0, 0), "RGB value");
+});
+
+test("light strong tonal blue", function() {
+  colour = new HSL(hue = 240, saturation = 0.75, lightness = 0.75);
+  same(colour.toRGB(), new RGB(143, 143, 239), "RGB value");
+});
+
+test("light weak tonal blue", function() {
+  colour = new HSL(hue = 240, saturation = 0.25, lightness = 0.875);
+  same(colour.toRGB(), new RGB(215, 215, 231), "RGB value");
+});
+
+test("dark strong tonal blue", function() {
+  colour = new HSL(hue = 240, saturation = 0.75, lightness = 0.125);
+  same(colour.toRGB(), new RGB(8, 8, 56), "RGB value");
+});
+
+test("dark weak tonal blue", function() {
+  colour = new HSL(hue = 240, saturation = 0.25, lightness = 0.25);
+  same(colour.toRGB(), new RGB(48, 48, 80), "RGB value");
+});
+
 // Magenta
 
-test("magenta", function() {
+test("pure magenta", function() {
   colour = new HSL(hue = 300, saturation = 1, lightness = 0.5);
   equals(colour.hue, hue, "Hue value");
   equals(colour.saturation, saturation, "Saturation value");
@@ -84,9 +359,64 @@ test("magenta", function() {
   same(colour.toRGB(opacity = 0.9), new RGB(255, 0, 255, opacity), "Explicitly translucent RGB value");
 });
 
+test("high saturation magenta", function() {
+  colour = new HSL(hue = 300, saturation = 0.75, lightness = 0.5);
+  same(colour.toRGB(), new RGB(223, 32, 223), "RGB value");
+});
+
+test("low saturation magenta", function() {
+  colour = new HSL(hue = 300, saturation = 0.25, lightness = 0.5);
+  same(colour.toRGB(), new RGB(159, 96, 159), "RGB value");
+});
+
+test("zero saturation magenta", function() {
+  colour = new HSL(hue = 300, saturation = 0, lightness = 0.5);
+  same(colour.toRGB(), new RGB(128, 128, 128), "RGB value");
+});
+
+test("full lightness magenta", function() {
+  colour = new HSL(hue = 300, saturation = 1, lightness = 1);
+  same(colour.toRGB(), new RGB(255, 255, 255), "RGB value");
+});
+
+test("high lightness magenta", function() {
+  colour = new HSL(hue = 300, saturation = 1, lightness = 0.875);
+  same(colour.toRGB(), new RGB(255, 191, 255), "RGB value");
+});
+
+test("low lightness magenta", function() {
+  colour = new HSL(hue = 300, saturation = 1, lightness = 0.125);
+  same(colour.toRGB(), new RGB(64, 0, 64), "RGB value");
+});
+
+test("zero lightness magenta", function() {
+  colour = new HSL(hue = 300, saturation = 1, lightness = 0);
+  same(colour.toRGB(), new RGB(0, 0, 0), "RGB value");
+});
+
+test("light strong tonal magenta", function() {
+  colour = new HSL(hue = 300, saturation = 0.75, lightness = 0.875);
+  same(colour.toRGB(), new RGB(247, 199, 247), "RGB value");
+});
+
+test("light weak tonal magenta", function() {
+  colour = new HSL(hue = 300, saturation = 0.25, lightness = 0.625);
+  same(colour.toRGB(), new RGB(183, 135, 183), "RGB value");
+});
+
+test("dark strong tonal magenta", function() {
+  colour = new HSL(hue = 300, saturation = 0.75, lightness = 0.25);
+  same(colour.toRGB(), new RGB(112, 16, 112), "RGB value");
+});
+
+test("dark weak tonal magenta", function() {
+  colour = new HSL(hue = 300, saturation = 0.25, lightness = 0.25);
+  same(colour.toRGB(), new RGB(80, 48, 80), "RGB value");
+});
+
 // White
 
-test("white", function() {
+test("pure white", function() {
   colour = new HSL(hue = 0, saturation = 0, lightness = 1);
   equals(colour.hue, hue, "Hue value");
   equals(colour.saturation, saturation, "Saturation value");
